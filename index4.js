5907e460eb2e14cbcc97ba450ad08e0820111c19
Misha Panivnyk <panivnykm@gmail.com>
2023-10-13 19:35:27 +0300

    fix code

--- index4.js
+++ index4.js
@@ -1,43 +1,35 @@
 function quickSort(arr, ascending) {
-  if (arr.length <= 1) {
-    return arr;
+  function partition(arr, low, high) {
+    const pivot = arr[high];
+    let i = low;
+
+    for (let j = low; j < high; j++) {
+      if ((ascending && arr[j] <= pivot) || (!ascending && arr[j] >= pivot)) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        i++;
+      }
+    }
+
+    [arr[i], arr[high]] = [arr[high], arr[i]];
+    return i;
   }
 
-  const pivot = arr[Math.floor(Math.random() * arr.length)];
-
-  const left = [];
-  const right = [];
-  const equal = [];
-
-  for (const element of arr) {
-    if (element < pivot) {
-      left.push(element);
-    } else if (element > pivot) {
-      right.push(element);
-    } else {
-      equal.push(element);
+  function sort(arr, low, high) {
+    if (low < high) {
+      const pi = partition(arr, low, high);
+      sort(arr, low, pi - 1);
+      sort(arr, pi + 1, high);
     }
   }
 
-  if (ascending) {
-    return [
-      ...quickSort(left, ascending),
-      ...equal,
-      ...quickSort(right, ascending),
-    ];
-  } else {
-    return [
-      ...quickSort(right, ascending),
-      ...equal,
-      ...quickSort(left, ascending),
-    ];
-  }
+  const low = 0;
+  const high = arr.length - 1;
+  sort(arr, low, high);
+
+  return arr;
 }
 
-// Приклад використання
-const arr = [8, 2, 11, 6, 9, 5, 12, 4];
-const ascendingSort = quickSort(arr.slice(), true);
-const descendingSort = quickSort(arr.slice(), false);
-
-console.log("За зростанням:", ascendingSort);
-console.log("За спаданням:", descendingSort);
+const arr = [5, 2, 9, 3, 6, 1, 8];
+const ascending = true;
+const sortedArr = quickSort(arr, ascending);
+console.log(sortedArr);
