cb6047f024741b62e0da27f21c9fbfcae89d7440
Misha Panivnyk <panivnykm@gmail.com>
2023-10-13 19:48:13 +0300

    matrix

--- index3.js
+++ index3.js
@@ -150,19 +150,141 @@ function findAndRemoveMaxElements(matrix) {
   }
 }
 
+function swapColumnsWithMaxAndMin(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  // Знаходження індексів стовпців з максимальним і мінімальним елементами
+  let maxColIndex = 0;
+  let minColIndex = 0;
+  let maxElement = matrix[0][0];
+  let minElement = matrix[0][0];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] > maxElement) {
+        maxElement = matrix[i][j];
+        maxColIndex = j;
+      }
+      if (matrix[i][j] < minElement) {
+        minElement = matrix[i][j];
+        minColIndex = j;
+      }
+    }
+  }
+
+  // Поміняти місцями стовпці з максимальним і мінімальним елементами
+  for (let i = 0; i < rows; i++) {
+    const temp = matrix[i][maxColIndex];
+    matrix[i][maxColIndex] = matrix[i][minColIndex];
+    matrix[i][minColIndex] = temp;
+  }
+}
+
+function findAndRemoveMaxValue(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let maxValue = matrix[0][0];
+  let maxRowIndex = 0;
+  let maxColIndex = 0;
+
+  // Знаходження максимального елементу та його позиції
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] > maxValue) {
+        maxValue = matrix[i][j];
+        maxRowIndex = i;
+        maxColIndex = j;
+      }
+    }
+  }
+
+  // Видалення рядка та стовпця, що містять максимальний елемент
+  matrix.splice(maxRowIndex, 1);
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].splice(maxColIndex, 1);
+  }
+}
+
+function findAndRemoveMultipleMaxValues(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let maxValues = [];
+  let maxRowIndex = [];
+
+  // Знаходження максимальних елементів та їх позицій
+  for (let i = 0; i < rows; i++) {
+    let rowMax = Math.max(...matrix[i]);
+    maxValues.push(rowMax);
+  }
+
+  let max = Math.max(...maxValues);
+
+  for (let i = 0; i < rows; i++) {
+    if (maxValues[i] === max) {
+      maxRowIndex.push(i);
+    }
+  }
+
+  // Видалення рядків, що містять максимуми
+  for (let i = maxRowIndex.length - 1; i >= 0; i--) {
+    matrix.splice(maxRowIndex[i], 1);
+  }
+}
+
+function swapRowAndColumnWithMaxAndMin(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  // Знаходження максимального та мінімального елементів
+  let maxElement = matrix[0][0];
+  let minElement = matrix[0][0];
+  let maxRowIndex = 0;
+  let maxColIndex = 0;
+  let minRowIndex = 0;
+  let minColIndex = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] > maxElement) {
+        maxElement = matrix[i][j];
+        maxRowIndex = i;
+        maxColIndex = j;
+      }
+      if (matrix[i][j] < minElement) {
+        minElement = matrix[i][j];
+        minRowIndex = i;
+        minColIndex = j;
+      }
+    }
+  }
+
+  // Поміняти місцями рядок та стовпець з максимумом та рядок та стовпець з мінімумом
+  const tempRow = matrix[maxRowIndex].slice();
+  matrix[maxRowIndex] = matrix[minRowIndex].slice();
+  matrix[minRowIndex] = tempRow;
+
+  for (let i = 0; i < rows; i++) {
+    const temp = matrix[i][maxColIndex];
+    matrix[i][maxColIndex] = matrix[i][minColIndex];
+    matrix[i][minColIndex] = temp;
+  }
+}
+
 const rows = 3;
 const cols = 3;
 const minValue = 10;
 const maxValue = 100;
-const k = 2; // Кількість позицій для зсуву
+const k = 0; // Кількість позицій для зсуву
 
 const matrix = createMatrix(rows, cols, minValue, maxValue);
 console.log("Початкова матриця:");
 printMatrix(matrix);
 
-// cyclicShiftMatrix(matrix, k);
-// console.log("Матриця після циклічного зсуву:");
-// printMatrix(matrix);
+cyclicShiftMatrix(matrix, k);
+console.log("Матриця після циклічного зсуву:");
+printMatrix(matrix);
 
 const sum = sumElementsAfterThird(matrix);
 console.log("Сума елементів після третього в кожному рядку:", sum);
@@ -173,8 +295,29 @@ console.log("Сума елементів після третього в кожн
 // );
 // printMatrix(matrix);
 
-findAndRemoveMaxElements(matrix);
+// findAndRemoveMaxElements(matrix);
+// console.log(
+//   "Матриця після видалення рядків та стовпців, що містять максимальні елементи:"
+// );
+// printMatrix(matrix);
+// swapColumnsWithMaxAndMin(matrix);
+// console.log(
+//   "Матриця після обміну стовпців з максимальним і мінімальним елементами:"
+// );
+// printMatrix(matrix);
+
+// findAndRemoveMaxValue(matrix);
+// console.log(
+//   "Матриця після видалення рядка та стовпця з максимальним елементом:"
+// );
+// printMatrix(matrix);
+
+// findAndRemoveMultipleMaxValues(matrix);
+// console.log("Матриця після видалення рядків, що містять багато максимумів:");
+// printMatrix(matrix);
+
+swapRowAndColumnWithMaxAndMin(matrix);
 console.log(
-  "Матриця після видалення рядків та стовпців, що містять максимальні елементи:"
+  "Матриця після обміну рядка і стовпця з максимумом та рядка і стовпця з мінімумом:"
 );
 printMatrix(matrix);
