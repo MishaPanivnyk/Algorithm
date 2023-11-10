01ee7c3283c6de131ca811c067c3eb4861174c04
Misha Panivnyk <panivnykm@gmail.com>
2023-11-10 17:30:54 +0200

    add comment

--- index9.js
+++ index9.js
@@ -1,9 +1,11 @@
+// Функція для генерації випадкового масиву заданого розміру
 function generateRandomArray(size) {
   const min = -10000;
   const max = 10000;
   const randomArray = [];
 
   for (let i = 0; i < size; i++) {
+    // Генерація випадкового числа у діапазоні від min до max
     const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     randomArray.push(randomNum);
   }
@@ -11,6 +13,7 @@ function generateRandomArray(size) {
   return randomArray;
 }
 
+// Спосіб 1: Сортування для знаходження шести найбільших елементів
 function findSixLargestSorting(arr) {
   if (arr.length <= 6) {
     return arr;
@@ -20,12 +23,14 @@ function findSixLargestSorting(arr) {
   for (let i = 0; i < 6; i++) {
     let maxIndex = i;
 
+    // Пошук найбільшого елемента залишкового масиву
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] > arr[maxIndex]) {
         maxIndex = j;
       }
     }
 
+    // Підміна елементів для сортування
     [arr[i], arr[maxIndex]] = [arr[maxIndex], arr[i]];
     largestSix.push(arr[i]);
   }
@@ -33,11 +38,14 @@ function findSixLargestSorting(arr) {
   return largestSix;
 }
 
+// Спосіб 2: Використання порівнянь для вставки елементів у масив шести найбільших
 function findSixLargestComparison(arr) {
   const largestSix = arr.slice(0, 6);
 
   for (let i = 6; i < arr.length; i++) {
     let current = arr[i];
+
+    // Порівняння та вставка нового елемента у відсортований масив шести найбільших
     if (current > largestSix[5]) {
       let j = 5;
       while (j >= 0 && current > largestSix[j]) {
@@ -51,43 +59,68 @@ function findSixLargestComparison(arr) {
   return largestSix;
 }
 
+// Спосіб 3: Порівняння та заміна для знаходження шести найбільших елементів
 function findSixLargestReplace(arr) {
   for (let i = 0; i < 6; i++) {
     let maxIndex = i;
     let max = arr[maxIndex];
+
+    // Пошук найбільшого елемента та його заміна
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] > max) {
         max = arr[j];
         maxIndex = j;
       }
     }
+
+    // Підміна елементів для знаходження шести найбільших
     [arr[i], arr[maxIndex]] = [arr[maxIndex], arr[i]];
   }
 
   return arr.slice(0, 6);
 }
 
+// Спосіб 4: QuickSelect для знаходження шести найбільших елементів
 function findSixLargestQuickSelect(arr) {
-  const largestSix = Array(6).fill(-Infinity);
+  const largestSix = arr.slice(0, 6).sort((a, b) => b - a);
 
-  for (let i = 0; i < arr.length; i++) {
-    const current = arr[i];
+  for (let i = 6; i < arr.length; i++) {
+    let current = arr[i];
 
+    // Використання бінарного пошуку для знаходження місця вставки нового елемента
     if (current > largestSix[5]) {
-      let j = 5;
+      let insertIndex = binarySearch(largestSix, current, 0, 5);
 
-      while (j >= 0 && current > largestSix[j]) {
-        largestSix[j + 1] = largestSix[j];
-        j--;
+      // Зсув елементів для вставки нового елемента
+      for (let j = 5; j > insertIndex; j--) {
+        largestSix[j] = largestSix[j - 1];
       }
 
-      largestSix[j + 1] = current;
+      largestSix[insertIndex] = current;
     }
   }
 
   return largestSix;
 }
 
+// Бінарний пошук використовується для знаходження місця вставки нового елемента
+function binarySearch(arr, target, start, end) {
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return start;
+}
+
+// Функція для вимірювання часу виконання заданої функції на заданому масиві
 function measureTime(func, arr) {
   const start = Date.now();
   func(arr);
@@ -95,8 +128,10 @@ function measureTime(func, arr) {
   return end - start;
 }
 
+// Розміри вхідних даних для вимірювань часу
 const inputSizes = [10000, 100000, 1000000, 10000000, 100000000];
 
+// Функція для виведення результатів вимірювань у вигляді таблиці
 function printTable(data) {
   console.log(
     "| Розмір вхідних даних | Спосіб 1 (Сортування) | Спосіб 2 (Масив) | Спосіб 3 (Максимальні елементи) | Спосіб 4 (QuickSelect) |"
@@ -112,8 +147,10 @@ function printTable(data) {
   });
 }
 
+// Масив для зберігання результатів вимірювань
 const resultData = [];
 
+// Проведення вимірювань для кожного розміру вхідних даних
 inputSizes.forEach((size) => {
   const randomArray = generateRandomArray(size);
 
@@ -122,6 +159,7 @@ inputSizes.forEach((size) => {
   const replaceTime = [];
   const quickSelectTime = [];
 
+  // Вимірювання часу для кожного методу три рази
   for (let i = 0; i < 3; i++) {
     sortingTime.push(measureTime(findSixLargestSorting, randomArray));
     comparisonTime.push(measureTime(findSixLargestComparison, randomArray));
@@ -129,6 +167,7 @@ inputSizes.forEach((size) => {
     quickSelectTime.push(measureTime(findSixLargestQuickSelect, randomArray));
   }
 
+  // Обчислення середнього часу для кожного методу
   let sumSortingTime = 0;
   for (let i = 0; i < sortingTime.length; i++) {
     sumSortingTime += sortingTime[i];
@@ -153,6 +192,7 @@ inputSizes.forEach((size) => {
   }
   const averageQuickSelectTime = sumQuickSelectTime / quickSelectTime.length;
 
+  // Додавання результатів до масиву
   resultData.push({
     size,
     method1: averageSortingTime.toFixed(2),
@@ -162,4 +202,5 @@ inputSizes.forEach((size) => {
   });
 });
 
+// Виведення результатів у вигляді таблиці
 printTable(resultData);
