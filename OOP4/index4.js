6ba932ad97c6df9351a82b6800e3c849b75c5d5d
Misha Panivnyk <panivnykm@gmail.com>
2024-03-18 13:49:15 +0200

    add OOP4

--- OOP4/index4.js
+++ OOP4/index4.js
@@ -1,18 +1,16 @@
-// Оголошення класу кімнат
 class Room {
   constructor(name, type, shape, params) {
     this.name = name;
     this.type = type;
     this.shape = shape;
-    this.params = params; // Додано параметри кімнати
+    this.params = params;
   }
 
-  // Метод для обчислення площі кімнати
   calculateArea() {
     let area = 0;
     switch (this.shape) {
       case "прямокутна":
-        area = this.params.length * this.params.width; // Використовуємо параметри кімнати
+        area = this.params.length * this.params.width;
         break;
       case "трикутна":
         area = 0.5 * this.params.base * this.params.height;
@@ -27,18 +25,15 @@ class Room {
   }
 }
 
-// Клас для квартири
 class Apartment {
   constructor() {
     this.rooms = [];
   }
 
-  // Додавання кімнати
   addRoom(room) {
     this.rooms.push(room);
   }
 
-  // Метод для обчислення загальної площі житлових кімнат
   getTotalLivingArea() {
     let totalArea = 0;
     this.rooms.forEach((room) => {
@@ -47,7 +42,6 @@ class Apartment {
     return totalArea;
   }
 
-  // Метод для обчислення загальної площі всіх кімнат
   getTotalArea() {
     let totalArea = 0;
     this.rooms.forEach((room) => {
@@ -56,7 +50,6 @@ class Apartment {
     return totalArea;
   }
 
-  // Метод для виведення інформації про кімнати
   listRooms() {
     this.rooms.forEach((room) => {
       console.log(
@@ -65,7 +58,6 @@ class Apartment {
     });
   }
 
-  // Метод для знаходження кількості кімнат кожної форми
   countRoomsByShape() {
     const roomCounts = {};
     this.rooms.forEach((room) => {
@@ -74,7 +66,6 @@ class Apartment {
     return roomCounts;
   }
 
-  // Метод для знаходження кількості кімнат кожного типу
   countRoomsByType() {
     const roomCounts = {};
     this.rooms.forEach((room) => {
@@ -83,34 +74,27 @@ class Apartment {
     return roomCounts;
   }
 
-  // Метод для отримання кількості витраченого газу у квартирі
   getGasUsage() {
-    // Припустимо, що дані про споживання газу генеруються випадково
-    return Math.floor(Math.random() * 100); // Випадкове число від 0 до 100
+    return Math.floor(Math.random() * 100);
   }
 
-  // Метод для отримання кількості витраченої води у квартирі
   getWaterUsage() {
-    // Припустимо, що дані про споживання води генеруються випадково
-    return Math.floor(Math.random() * 100); // Випадкове число від 0 до 100
+    return Math.floor(Math.random() * 100);
   }
 }
 
-// Оголошення класу типу кімнати
 class RoomType {
   constructor(name, description, isLiving) {
     this.name = name;
     this.description = description;
-    this.isLiving = isLiving; // Вказує, чи є ця кімната житловою
+    this.isLiving = isLiving;
   }
 }
 
-// Встановлюємо типи кімнат
 const livingRoomType = new RoomType("Вітальня", "Житлова кімната", true);
 const kitchenRoomType = new RoomType("Кухня", "Кухонна кімната", false);
 const bathroomRoomType = new RoomType("Ванна", "Ванна кімната", false);
 
-// Створюємо кімнати
 const room1 = new Room("Кімната 1", livingRoomType, "прямокутна", {
   length: 5,
   width: 4,
@@ -121,13 +105,11 @@ const room2 = new Room("Кімната 2", kitchenRoomType, "трикутна", 
 });
 const room3 = new Room("Кімната 3", bathroomRoomType, "кругла", { radius: 3 });
 
-// Створюємо квартиру та додаємо кімнати
 const apartment = new Apartment();
 apartment.addRoom(room1);
 apartment.addRoom(room2);
 apartment.addRoom(room3);
 
-// Виводимо результати
 console.log("Загальна площа житлових кімнат:", apartment.getTotalLivingArea());
 console.log("Загальна площа всіх кімнат:", apartment.getTotalArea());
 console.log("Перелік кімнат:");
